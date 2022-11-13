4000bf6b34efc05751e9b16455529c0fbf9d0fe6
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-11-13 12:57:56 +0100

    test

--- routes/root.js
+++ routes/root.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("goodday!");
+router.get('/', (req, res) => {
+  res.send('yeiii!');
 });
 
 module.exports = router;
