f60d6c314f9d6e4294ff3825b9b585bef4ede798
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-11-15 08:58:01 +0100

    added environment variable

--- startup/database.js
+++ startup/database.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function () {
-  const database = config.get('database.development');
+  // choosing database depends on the current environment
+  let environment = process.env.NODE_ENV;
+  if (typeof environment === 'undefined') environment = 'development';
+  console.log(`environment: ${environment}`);
+
+  const configuration = 'database.' + environment;
+  const database = config.get(configuration);
   mongoose
     .connect(database)
     .then(() => console.log(`connected to ${database}`))
