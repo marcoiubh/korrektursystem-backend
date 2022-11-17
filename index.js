87089e0b14d8e17e3a95d3632ef88a16614e0739
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-11-17 21:18:38 +0100

    fixed issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const app = express();
 const config = require('config');
 const express = require('express');
+const app = express();
 
 // making sure the environment variable is set or quit
 // export kms_jwtPrivateKey=1234
