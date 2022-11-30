e1bae3ece79c44eac8cb445541c537d99e38cf9a
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-11-30 18:05:09 +0100

    test with wrong origin url

--- startup/cors.js
+++ startup/cors.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 module.exports = function (app) {
   app.use(
     cors({
-      origin: 'https://www.korrektursystem.live',
+      origin: 'https://www.koektursystem.live',
     })
   );
 };
