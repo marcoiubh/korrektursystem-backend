88f292f6a8aa3b299dc5ce5dc7852b8494b75537
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-12-01 17:57:11 +0100

    fix: add localhost to policy

--- startup/cors.js
+++ startup/cors.js
@@ -2,7 +2,10 @@ const cors = require('cors');
 module.exports = function (app) {
   app.use(
     cors({
-      origin: 'https://www.korrektursystem.live',
+      origin: [
+        'https://www.korrektursystem.live',
+        'http://localhost:3000',
+      ],
     })
   );
 };
