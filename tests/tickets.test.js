6644bfbd5310845dfd718d36aba4427fc9a4bd18
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-12-13 18:45:58 +0100

    test: completed ticket tests & add coverage report

--- tests/tickets.test.js
+++ tests/tickets.test.js
@@ -7,34 +7,77 @@ const jwt = require('jsonwebtoken');
 let token = {};
 
 describe('/tickets', () => {
-  it('should return 401 "Unauthorized"', async () => {
-    const res = await request(app).get('/tickets').expect(401);
+  describe('GET', () => {
+    it('should return 401 "Unauthorized" without passing a valid token', async () => {
+      const res = await request(app).get('/tickets').expect(401);
+    });
+
+    it('should return 200 "OK" when authentification succeeds', async () => {
+      const res = await request(app)
+        .get('/tickets')
+        .set('x-auth-token', token)
+        .expect(200);
+    });
+
+    it('should return true with at least one _id in the database', async () => {
+      const res = await request(app)
+        .get('/tickets')
+        .set('x-auth-token', token);
+      expect(res.body.some(({ _id }) => _id)).toBe(true);
+    });
   });
 
-  it('should return 200 "OK"', async () => {
-    const res = await request(app)
-      .get('/tickets')
-      .set('x-auth-token', token)
-      .expect(200);
+  describe('POST', () => {
+    it('should return 401 "Unauthorized" without passing a valid token', async () => {
+      const res = await request(app).post('/tickets').expect(401);
+    });
+
+    it('should return 200 "OK" when authentification succeeds', async () => {
+      const res = await request(app)
+        .post('/tickets')
+        .set('x-auth-token', token)
+        .expect(200);
+    });
+    it('should return new object with an _id property', async () => {
+      const res = await request(app)
+        .post('/tickets')
+        .set('x-auth-token', token);
+      expect(res.body).toHaveProperty('_id');
+    });
+    it('should return new object with the title "title01" when an object with that title has been saved', async () => {
+      const res = await request(app)
+        .post('/tickets')
+        .set('x-auth-token', token)
+        .send({ title: 'title01' });
+      expect(res.body).toEqual(
+        expect.objectContaining({ title: 'title01' })
+      );
+    });
   });
 
-  // it('should return at least one required ticket property', async () => {
-  //   const res = await request(app)
-  //     .get('/tickets')
-  //     .set('x-auth-token', token);
-  //   // expect(res.body).toEqual(except.arrayContaining).toHaveProperty('title');
-  //   expect(res.body).toEqual(
-  //     expect.arrayContaining([
-  //       expect.objectContaining({ title: 'title01' }),
-  //     ])
-  //   );
-  // });
+  describe('PUT', () => {
+    it('should return 401 "Unauthorized"  without passing a valid token', async () => {
+      const res = await request(app)
+        .put('/tickets/639863a90faa1b48a59a1a2d')
+        .expect(401);
+    });
 
-  it('should return true with at least one _id in the database', async () => {
-    const res = await request(app)
-      .get('/tickets')
-      .set('x-auth-token', token);
-    expect(res.body.some(({ _id }) => _id)).toBe(true);
+    it('should return 200 "OK" when authentification succeeds', async () => {
+      const res = await request(app)
+        .put('/tickets/639863a90faa1b48a59a1a2d')
+        .set('x-auth-token', token)
+        .expect(200);
+    });
+    it('should update the objects status to "Closed"', async () => {
+      const res = await request(app)
+        .put('/tickets/639863a90faa1b48a59a1a2d')
+        .set('x-auth-token', token)
+        .send({ status: 'Closed' });
+
+      expect(res.body).toEqual(
+        expect.objectContaining({ status: 'Closed' })
+      );
+    });
   });
 });
 
