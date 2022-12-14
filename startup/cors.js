9ba8a6abc404e482fd5a4f649409ff5917fb74f9
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-12-14 23:13:15 +0100

    modify: cors policy

--- startup/cors.js
+++ startup/cors.js
@@ -1,12 +1,13 @@
 const cors = require('cors');
 module.exports = function (app) {
-  app.use(
-    cors({
-      origin: [
-        'https://www.korrektursystem.live',
-        'https://korrektursystem.live',
-        'http://localhost:3000',
-      ],
-    })
-  );
+  app.use(cors());
+  // app.use(
+  //   cors({
+  //     origin: [
+  //       'https://www.korrektursystem.live',
+  //       'https://korrektursystem.live',
+  //       'http://localhost:3000',
+  //     ],
+  //   })
+  // );
 };
