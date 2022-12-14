245601bd49bf2a5a2d539bda053993e5e9c8ac60
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-12-14 23:19:01 +0100

    livetest: cors policy

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ try {
 require('./startup/cors')(app);
 require('./startup/database')();
 require('./startup/routes')(app);
-require('./startup/startServer')(app);
+// require('./startup/startServer')(app);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
+});
 
 module.exports = app;
