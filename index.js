b67a0bda4b1fb92060e8622e1e00d0c7d2bf8f08
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-12-14 23:32:32 +0100

    fix: cors policy bug

--- index.js
+++ index.js
@@ -14,9 +14,4 @@ require('./startup/database')();
 require('./startup/routes')(app);
 require('./startup/startServer')(app);
 
-// const port = process.env.PORT || 4000;
-// app.listen(port, () => {
-//   console.log(`listening on port ${port}...`);
-// });
-
 module.exports = app;
