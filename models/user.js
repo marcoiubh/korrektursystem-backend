907543f7be57142041cceca8e95668d3a2b5fa33
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-12-19 18:27:31 +0100

    test: userModel with strict rules

--- models/user.js
+++ models/user.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema(
   {
-    email: { type: String, unique: true },
-    password: String,
-    role: { type: String, enum: ['student', 'professor'] },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ['student', 'professor'],
+      required: true,
+    },
     modules: [
       {
         type: mongoose.Schema.Types.ObjectId,
---
+++ tests/unitTests/userModel.test.js
@@ -0,0 +1,37 @@
+const { expect } = require('chai');
+
+const { User } = require('../../models/user');
+
+describe('User Model', () => {
+  it('should be invalid if required field is missing', () => {
+    let user = new User({});
+    user.validate((error) => {
+      expect(error.errors.email).to.exist;
+      expect(error.errors.password).to.exist;
+      expect(error.errors.role).to.exist;
+    });
+  });
+
+  it('should be invalid if role is not student or professor', () => {
+    let user = new User({
+      email: 'test',
+      password: 'secret',
+      role: 'wrong',
+    });
+
+    user.validate((error) => {
+      expect(error.errors.role).to.exist;
+    });
+  });
+
+  it('should not throw errors with valid fields', () => {
+    let user = new User({
+      email: 'test',
+      password: 'secret',
+      role: 'student',
+    });
+    user.validate((error) => {
+      expect(error).to.not.exist;
+    });
+  });
+});
